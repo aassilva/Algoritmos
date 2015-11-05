ec5831486e33d17e8659586508dc0ddc2b44c2a8
alunoinf <alunoinf@pc-649301.(none)>
2015-11-05 19:08:18 -0200

    Update

--- algoritmo6/src/test/java/br/ufg/inf/es/construcao/algoritmo6/Algoritmo6Test.java
+++ algoritmo6/src/test/java/br/ufg/inf/es/construcao/algoritmo6/Algoritmo6Test.java
@@ -1,4 +1,4 @@
-package home.alunoinf.avaliacao.algoritmo6;
+package br.ufg.inf.es.construcao.algoritmo6;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -7,18 +7,18 @@ public class Algoritmo6Test {
 
     @Test(expected = IllegalArgumentException.class)
     public void testPrimeiroParametroInvalido() throws Exception {
-        Algoritmo6.produto(-10, 10);
+        Algoritmo6.potenciaSoma(-10, 10);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testsegundoParametroInvalido() throws Exception {
-        Algoritmo6.produto(15, 0);
+        Algoritmo6.potenciaSoma(15, 0);
     }
 
     @Test
     public void testCasoTrivial() {
-        Assert.assertEquals(1000, Algoritmo6.produto(10, 3));
-        Assert.assertEquals(25, Algoritmo6.produto(5, 2));
-        Assert.assertEquals(243, Algoritmo6.produto(3, 5));
+        Assert.assertEquals(1000, Algoritmo6.potenciaSoma(10, 3));
+        Assert.assertEquals(25, Algoritmo6.potenciaSoma(5, 2));
+        Assert.assertEquals(243, Algoritmo6.potenciaSoma(3, 5));
     }
 }
